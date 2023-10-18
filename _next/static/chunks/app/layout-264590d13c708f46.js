c40203eec508859582c394f3f8a418e8574d8d1c
oleniuh <maxim.oleniukh@gmail.com>
2023-10-18 06:55:30 +0100

    Updates

---
+++ _next/static/chunks/app/layout-264590d13c708f46.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{8687:function(n,e,t){Promise.resolve().then(t.t.bind(t,9968,23)),Promise.resolve().then(t.t.bind(t,3107,23))},3107:function(){},9968:function(){}},function(n){n.O(0,[971,864,744],function(){return n(n.s=8687)}),_N_E=n.O()}]);
--- components/LoginButton.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const LoginButton = () => {
-  return (
-      <button className="login-btn">Login</button>
-  )
-}
-
-export default LoginButton;
