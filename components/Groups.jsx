7b8124fd70cd94011e15c39c118f5d021c3a1dc7
oleniuh <maxim.oleniukh@gmail.com>
2023-10-18 14:10:23 +0100

    path fix

--- components/Groups.jsx
+++ components/Groups.jsx
@@ -3,31 +3,31 @@ import Group from './Group';
 
 const groupData = [
   {
-    imageUrl: "/images/avatar-6.jpg",
+    imageUrl: "./images/avatar-6.jpg",
     title: "Street Artists",
     memberCount: "4",
     isPublic: true,
   },
   {
-    imageUrl: "/images/avatar-7.jpg",
+    imageUrl: "./images/avatar-7.jpg",
     title: "Cosplayers of the World",
     memberCount: "3",
     isPublic: true,
   },
   {
-    imageUrl: "/images/avatar-8.jpg",
+    imageUrl: "./images/avatar-8.jpg",
     title: "Stream Designers",
     memberCount: "3",
     isPublic: true,
   },
   {
-    imageUrl: "/images/avatar-9.jpg",
+    imageUrl: "./images/avatar-9.jpg",
     title: "Gaming Watchtower",
     memberCount: "3",
     isPublic: false,
   },
   {
-    imageUrl: "/images/avatar-10.jpg",
+    imageUrl: "./images/avatar-10.jpg",
     title: "Living in Japan",
     memberCount: "2",
     isPublic: true,
--- components/Members.jsx
+++ components/Members.jsx
@@ -2,31 +2,31 @@ import React from "react";
 import Member from "./Member";
 const membersData = [
   {
-    imageUrl: "/images/avatar-1.avif",
+    imageUrl: "./images/avatar-1.avif",
     title: "Sandra Strange",
     subtitle: "@sanstrange",
     isMember: true,
   },
   {
-    imageUrl: "/images/avatar-2.jpg",
+    imageUrl: "./images/avatar-2.jpg",
     title: "Rosie Sakura",
     subtitle: "@sakuraro", 
     isMember: false,
   },
   {
-    imageUrl: "/images/avatar-3.jpg",
+    imageUrl: "./images/avatar-3.jpg",
     title: "Damian Greyson",
     subtitle: "@dgreyson",
     isMember: false,
   },
   {
-    imageUrl: "/images/avatar-4.jpg",
+    imageUrl: "./images/avatar-4.jpg",
     title: "The Green Goo",
     subtitle: "@greengoo",
     isMember: false,
   },
   {
-    imageUrl: "/images/avatar-5.jpg",
+    imageUrl: "./images/avatar-5.jpg",
     title: "Bearded Wonder",
     subtitle: "@brdwonder",
     isMember: false,
